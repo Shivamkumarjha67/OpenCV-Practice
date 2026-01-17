158576e963c2a86fa1d3d5841f4240bd4408655f
Shivam Kumar Jha <shkrjh@gmail.com>
2026-01-17 19:24:01 +0530

    Adding the basic of Open-CV practices and image datasets.

---
+++ BASIC_CV/.ipynb_checkpoints/Drawing_shape_on_image-checkpoint.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+# Defining the function
+def red_circle(event, x, y, flags, param):
+    if event == cv2.EVENT_RBUTTONDOWN:
+        cv2.circle(img, (x, y), 100, (0, 0, 255), thickness=5)
+
+cv2.namedWindow(winname = 'Dog Backpack')
+cv2.setMouseCallback('Dog Backpack', red_circle)
+
+
+# Showing image with open-cv
+img = cv2.imread('../DATA/dog_backpack.jpg')
+
+while True:
+    cv2.imshow('Dog Backpack', img)
+
+    if cv2.waitKey(15) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
